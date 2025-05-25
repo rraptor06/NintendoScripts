75c8aa98362063b9ae8322f5fb90fada82110eaa
Raptor06 <137603383+rraptor06@users.noreply.github.com>
2025-05-25 17:44:41 +0200

    Update login_friends.py

--- login_friends.py
+++ login_friends.py
@@ -4,14 +4,17 @@ import anyio
 import logging
 logging.basicConfig(level=logging.INFO)
 
+ACCESS_KEY: str = "ridfebb9"
+NEX_VERSION: int = 200
 
 async def main():
 	s = settings.load("friends")
-	s.configure("ridfebb9", 20000)
+	s.configure(ACCESS_KEY, NEX_VERSION)
 	
 	async with backend.connect(s, "212.227.58.3", 6000) as be:
 		async with be.login_guest() as client:
 			logging.info("WORKING IG.")
 			pass
+
 
 anyio.run(main)
