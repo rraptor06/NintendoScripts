e26bfb7c008f402ea7baa2101f949c249beebcd7
Raptor06 <137603383+rraptor06@users.noreply.github.com>
2025-07-15 19:08:16 +0200

    Create test_rmc.py

---
+++ tests/test_rmc.py
@@ -0,0 +1,26 @@
+from nintendo.nex import settings, rmc
+
+
+if __name__ == "__main__":
+    s = settings.default()
+    msg = rmc.RMCMessage(s)
+
+    msg.protocol = 1
+    msg.mode = msg.REQUEST
+    msg.call_id = 42
+    msg.method = 123
+    msg.body = b"Hello, world!"
+
+    encoded_data = msg.encode()
+    print(f"Encoded data bytes: {encoded_data}")
+
+    decoded_msg = rmc.RMCMessage(s)
+    decoded_msg.decode(encoded_data)
+
+    print("Message décodé :")
+    print("Protocol:", decoded_msg.protocol)
+    print("Mode:", "REQUEST" if decoded_msg.mode == decoded_msg.REQUEST else "RESPONSE")
+    print("Call ID:", decoded_msg.call_id)
+    print("Method:", decoded_msg.method)
+    print("Body:", decoded_msg.body)
+  
